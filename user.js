13fc287e2fc7d41e6ce675f28a73b079e26a71e1
hiep Hoang <hiepvn4@gmail.com>
2025-11-18 14:00:30 +0700

    Cập nhật a

--- user.js
+++ user.js
@@ -21,7 +21,7 @@ app.use(express.json());
 let db;
 async function connectDB() {
   try {
-    db = await mysql.createConnection({
+    db = await mysql.createPool({
     host: 'sql12.freesqldatabase.com',  // host FreeSQL
     user: 'sql12808282',                // username FreeSQL
     password: 'ssJaXSuIdK',  // password FreeSQL
